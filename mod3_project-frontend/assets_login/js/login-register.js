55e8fbbff28ded051aa5d2588e6fc03cd3f59b8e
github username <github email address>
2019-05-28 21:24:09 +0100

    Built login functionality on login-register.js

--- mod3_project-frontend/assets_login/js/login-register.js
+++ mod3_project-frontend/assets_login/js/login-register.js
@@ -4,48 +4,55 @@
  * Autor: Creative Tim
  * Web-autor: creative.tim
  * Web script: http://creative-tim.com
- * 
+ *
  */
-function showRegisterForm(){
-    $('.loginBox').fadeOut('fast',function(){
-        $('.registerBox').fadeIn('fast');
-        $('.login-footer').fadeOut('fast',function(){
-            $('.register-footer').fadeIn('fast');
-        });
-        $('.modal-title').html('Register with your email address');
-    }); 
-    $('.error').removeClass('alert alert-danger').html('');
-       
+
+const formEl = document.querySelector("form");
+const USER_URL = "http://localhost:3000/users";
+const SESSIONS_URL = "http://localhost:3000/sessions";
+let currentUserId = null;
+
+function showRegisterForm() {
+  $(".loginBox").fadeOut("fast", function() {
+    $(".registerBox").fadeIn("fast");
+    $(".login-footer").fadeOut("fast", function() {
+      $(".register-footer").fadeIn("fast");
+    });
+    $(".modal-title").html("Register with your email address");
+  });
+  $(".error")
+    .removeClass("alert alert-danger")
+    .html("");
 }
-function showLoginForm(){
-    $('#loginModal .registerBox').fadeOut('fast',function(){
-        $('.loginBox').fadeIn('fast');
-        $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');    
-        });
-        
-        $('.modal-title').html('Login with your email address');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+function showLoginForm() {
+  $("#loginModal .registerBox").fadeOut("fast", function() {
+    $(".loginBox").fadeIn("fast");
+    $(".register-footer").fadeOut("fast", function() {
+      $(".login-footer").fadeIn("fast");
+    });
+
+    $(".modal-title").html("Login with your email address");
+  });
+  $(".error")
+    .removeClass("alert alert-danger")
+    .html("");
 }
 
-function openLoginModal(){
-    showLoginForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
-    
+function openLoginModal() {
+  showLoginForm();
+  setTimeout(function() {
+    $("#loginModal").modal("show");
+  }, 230);
 }
-function openRegisterModal(){
-    showRegisterForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
-    
+function openRegisterModal() {
+  showRegisterForm();
+  setTimeout(function() {
+    $("#loginModal").modal("show");
+  }, 230);
 }
 
-function loginAjax(){
-    /*   Remove this comments when moving to server
+function loginAjax() {
+  /*   Remove this comments when moving to server
     $.post( "/login", function( data ) {
             if(data == 1){
                 window.location.replace("/home");            
@@ -55,17 +62,42 @@ function loginAjax(){
         });
     */
 
-/*   Simulate error message from the server   */
-     shakeModal();
+  /*   Simulate error message from the server   */
+  shakeModal();
 }
 
-function shakeModal(){
-    $('#loginModal .modal-dialog').addClass('shake');
-             $('.error').addClass('alert alert-danger').html("Invalid email! Try again.");
-             $('input[type="password"]').val('');
-             setTimeout( function(){ 
-                $('#loginModal .modal-dialog').removeClass('shake'); 
-    }, 1000 ); 
+function shakeModal() {
+  $("#loginModal .modal-dialog").addClass("shake");
+  $(".error")
+    .addClass("alert alert-danger")
+    .html("Invalid email! Try again.");
+  $('input[type="password"]').val("");
+  setTimeout(function() {
+    $("#loginModal .modal-dialog").removeClass("shake");
+  }, 1000);
 }
 
-   
+const login = () => {
+  const userEmail = formEl.querySelector("#email").value;
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: userEmail })
+  };
+  return fetch(SESSIONS_URL, options)
+    .then(response => response.json())
+    .then(id => (currentUserId = id))
+    .then(window.location.replace("./index.html"));
+};
+
+const listenToForm = () => {
+  formEl.addEventListener("submit", e => {
+    e.preventDefault(), login(SESSIONS_URL);
+  });
+};
+
+const init = () => {
+  listenToForm();
+};
+
+init();
