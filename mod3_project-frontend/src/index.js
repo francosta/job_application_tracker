d007b6422781c675d949f5daae0d2638d248257e
endyranaudo <endyranaudo@gmail.com>
2019-05-28 15:48:27 +0100

    fixed loading console errors

--- mod3_project-frontend/src/index.js
+++ mod3_project-frontend/src/index.js
@@ -1,1 +1,13 @@
 // alert("Test")
+const userURL = "http://localhost:3000/users"
+
+// const ongoingApplications = document.getElementById("ongoing-applications")
+// ongoingApplications.innerText = "TEST ONGOING"
+
+function getUsers(userURL) {
+  return fetch(userURL).then(resp => resp.json());
+}
+
+function renderOngoingApplications(users) {
+  const ongoingApplications = document.getElementById("ongoing-applications")
+}
