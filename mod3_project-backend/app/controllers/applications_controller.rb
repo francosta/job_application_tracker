50c548d8a86578d8d6289795a74733ab272af9eb
github username <github email address>
2019-05-29 16:39:16 +0100

    Added all the application API bs

--- mod3_project-backend/app/controllers/applications_controller.rb
+++ mod3_project-backend/app/controllers/applications_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationsController < ApplicationController
+
+    def index
+        @applications = Application.all
+        render json: ApplicationSerializer.new(@applications).to_serialized_hash
+    end
+
+    def show
+        @application = Application.find_by(id: params[:id])
+        render json: ApplicationSerializer.new(@application).to_serialized_hash
+    end
+
+    def update
+        application = Application.find_by(id: params[:id].to_i)
+        if application.update(application_params)
+            render json: application
+        end
+    end
+
+    private
+
+    def application_params
+        params.permit(:company_name, :person_of_contact, :role)
+    end
+
+
 end
---
+++ mod3_project-backend/app/services/application_serializer.rb
@@ -0,0 +1,22 @@
+class ApplicationSerializer
+
+    def initialize(application_object)
+        @application = application_object
+    end
+
+    def to_serialized_hash
+        options = {            
+              include: {
+                  tasks: {
+                      only: [:name, :deadline]
+                  },
+                  cover_letters: {
+                    only: [:content]
+                  }
+                }
+          }
+        @application.to_json(options)
+    end
+
+
+end
