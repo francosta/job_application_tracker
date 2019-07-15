568fd3ce214f5d29cafb5800117aef80697580c3
francosta <francisco@fcosta.pt>
2019-07-15 11:23:46 +0100

    Added city to users.

--- mod3_project-backend/db/schema.rb
+++ mod3_project-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_122904) do
+ActiveRecord::Schema.define(version: 2019_07_15_102305) do
 
   create_table "applications", force: :cascade do |t|
     t.string "person_of_contact"
